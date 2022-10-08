1361f36b5218d3ab9a523daa420fcc2bb3c8bfc1
P129BFC <P129BFC@L-PW02X092>
2022-10-08 09:38:01 +0400

    Add project files.

---
+++ labsheet1/Class1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labsheet1
+{
+    public class Labsheet1
+    {
+
+        public static void Display()
+        {
+            int previous = 0;
+            foreach (var cat in Enum.GetValues(typeof(Category)))
+            {
+                Console.WriteLine("Category" + cat + "has the range of" + previous + " - " + (int)cat);
+                previous = (int)cat + 1;
+
+            }
+
+        }
+
+
+        public static void Check()
+        {
+            int value = 0;
+
+            do
+            {
+                Display();
+                Console.Write("Enter a value: ");
+                value = int.Parse(Console.ReadLine());
+                Category userCategory = new Category();
+
+
+
+
+
+                int i = 0;
+                foreach (int cat in Enum.GetValues(typeof(Category)))
+                {
+                    int previousValue = 0;
+                    previousValue = (int)cat + 1;
+                    if (i == 0)
+                    {
+                        //userCategory = cat;
+
+                        if (cat >= previousValue && cat <= value)
+                        {
+                            Console.WriteLine("Your category: " + userCategory);
+                            previousValue = value + 1;
+                        }
+
+
+                    }
+
+                }
+
+            } while (value != 1);
+
+
+        }
+    }
+}
---
+++ session1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using lab1Library;
+
+
+namespace session1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            labsheet1.Check();
+
+            Console.ReadLine();
+
+        }
+    }
+}
